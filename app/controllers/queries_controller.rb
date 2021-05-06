179bcf676844c2ea266108661fc3a6c24a99a344
Shinan Liao <shinanliao1@gmail.com>
2021-05-06 12:21:25 -0700

    adding guessing number method

--- app/controllers/queries_controller.rb
+++ app/controllers/queries_controller.rb
@@ -10,4 +10,17 @@ class QueriesController < ApplicationController
       render json: {message: "Hey, your name starts with the first letter of the alphabet!"}
     end
   end
+
+  def guess_number_query
+    user_guess = params[:user_guess].to_i
+    winning number = 36
+    if user_guess > winning number
+      output = "Too high!"
+    elsif user_guess < winning number
+      output = "Too low!"
+    else
+      output = "You win!"
+    end
+    render json: {message: output_message}
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   get "/name" => "queries#name_query"
   get "/first_letter" => "queries#first_letter_query"
+  get "/number" => "queries#guess_number_query"
 end
