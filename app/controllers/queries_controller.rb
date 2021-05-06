ca6688a842f6b4d462c3d97cf9c20463f67cffe2
Shinan Liao <shinanliao1@gmail.com>
2021-05-06 11:55:37 -0700

    adding second method if first letter of name starts with a

--- app/controllers/queries_controller.rb
+++ app/controllers/queries_controller.rb
@@ -3,4 +3,11 @@ class QueriesController < ApplicationController
     name = params[:name]
     render json: {message: "#{name}".upcase}
   end
+
+  def first_letter_query
+    name = params[:name]
+    if name[0] == "a"
+      render json: {message: "Hey, your name starts with the first letter of the alphabet!"}
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  get "/query_params" => "queries#name_query"
+  get "/name" => "queries#name_query"
+  get "/first_letter" => "queries#first_letter_query"
 end
