b772a8c0eadbb32fa6682c5c91e37cc737342f0f
Shinan Liao <shinanliao1@gmail.com>
2021-05-06 13:10:42 -0700

    merging name queries

--- app/controllers/queries_controller.rb
+++ app/controllers/queries_controller.rb
@@ -1,14 +1,11 @@
 class QueriesController < ApplicationController
-  def name_query
-    name = params[:name]
-    render json: {message: "#{name}".upcase}
-  end
-
-  def first_letter_query
-    name = params[:name]
-    if name[0] == "a"
-      render json: {message: "Hey, your name starts with the first letter of the alphabet!"}
+  def query_name
+    user_name = params[:name].upcase
+    output_message = "Your name is #{user_name}"
+    if user_name.starts_with?("A")
+      output_message = "Hey, your name starts with the first letter of the alphabet!"
     end
+    render json: {message: output_message}
   end
 
   def guess_number_query
