f8b35b9156c1665ad01af7b0ead49fcd6c1e8394
Shinan Liao <shinanliao1@gmail.com>
2021-05-07 12:30:33 -0700

    adding new queries and posts

--- app/controllers/queries_controller.rb
+++ app/controllers/queries_controller.rb
@@ -1,23 +1,43 @@
 class QueriesController < ApplicationController
+
   def query_name
-    user_name = params[:name].upcase
-    output_message = "Your name is #{user_name}"
+    user_name = params[:name]
+    output_message = "Your name is #{user_name}.upcase"
     if user_name.starts_with?("A")
       output_message = "Hey, your name starts with the first letter of the alphabet!"
     end
     render json: {message: output_message}
   end
 
-  def guess_number_query
+  def url_params
+    user_message = params[:my_message]
+    render json: {message: "Your message is #{user_message}"}
+  end
+
+  def guess
     user_guess = params[:user_guess].to_i
-    winning number = 36
-    if user_guess > winning number
-      output = "Too high!"
-    elsif user_guess < winning number
-      output = "Too low!"
+    winning_number = 36
+    if user_guess > winning_number
+      output_message = "You guessed too high!"
+    elsif user_guess < winning_number
+      output_message = "You guessed too low!"
     else
-      output = "You win!"
+      output_message = "You win!"
     end
     render json: {message: output_message}
+  end
+
+
+  def username_params
+    username = params[:username]
+    password = params[:password]
+    right_username = "hugh"
+    right_password = "swordfish"
+    if username == right_username && password == right_password
+      output_message = "Valid credentials"
+    else
+      output_message = "Invalid credentials"
+    end
+    render json: {message: output_message}  
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  get "/name" => "queries#name_query"
-  get "/number" => "queries#guess_number_query"
+  get "/query_name" => "queries#query_name"
+  get "/query_guess" => "queries#query_guess"
+  get "/query_guess" => "queries#guess"
+
+  get "/url_params/:my_message" => "queries#url_params"
+  get "/url_guess/:user_guess" => "queries#guess"
+  post "/body_params" => "queries#guess"
+  post "/username_params" => "queries#username_params"
 end
